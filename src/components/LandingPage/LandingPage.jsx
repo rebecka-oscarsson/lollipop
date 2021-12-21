f01b190d1687926f595e2290a8f21868b1a9cf01
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-12-21 18:56:00 +0100

    added elements (logo, headlines and button)

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,26 @@
-import styles from './LandingPage.module.scss'
+import styles from "./LandingPage.module.scss";
+import logoMobile from "../../images/logoMobile.svg";
+import logo from "../../images/logo.svg";
+
 function LandingPage() {
-    return <div className={styles.LandingPage}>LandingPage</div>;
+  return (
+    <section className={styles.LandingPage}>
+      <picture>
+        <source
+          media="(max-width:480px)"
+          srcSet={logoMobile}
+        />
+        <source
+          media="(min-width:481px)"
+          srcSet={logo}
+        />
+        <img src={logo} alt="company logo" />
+      </picture>
+      <h2>Om du vill vara trendig - och äta gott!</h2>
+      <h1>Grand Opening 23/12</h1>
+      <button>boka nu</button>
+    </section>
+  );
 }
 
 export default LandingPage;
