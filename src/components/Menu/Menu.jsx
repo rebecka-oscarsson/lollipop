9c3b4657dd2165f900312725d5661ba3f99bdf75
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-12-21 15:10:31 +0100

    Style "Boka bord"-btn

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,42 @@
-import styles from "./Menu.module.scss";
+import { useState } from 'react';
+import styles from './Menu.module.scss';
 function Menu() {
-  return (
-    <div className={styles.Menu}>
-      Menu
-      <span className={styles.Line1}></span>
-      <span className={styles.Line2}></span>
-      <span className={styles.Line3}></span>
-    </div>
-  );
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    return (
+        <div className={styles.Menu}>
+            {menuOpen ? (
+                <div className={styles.MenuOpen}>
+                    <div onClick={() => setMenuOpen(false)}>Close menu</div>
+                    <div>
+                        <div className={styles.MenuHeader}>Menu</div>
+                        Om Oss <br />
+                        Våra rätter <br />
+                        Kontakt <br />
+                        Hitta till oss <br />
+                        Catering <br />
+                        Take Away <br />
+                        <button className={styles.BokaBordBtn}>
+                            Boka bord
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.MenuClosed}>
+                    <div
+                        className={styles.MenuLines}
+                        onClick={() => setMenuOpen(true)}
+                    >
+                        <div className={styles.LinesContainer}>
+                            <div className={styles.Line1}></div>
+                            <div className={styles.Line2}></div>
+                            <div className={styles.Line3}></div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Menu;
