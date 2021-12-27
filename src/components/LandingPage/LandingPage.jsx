4b183a83e61207701047d7df518187b6eed66afa
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-12-27 12:17:34 +0100

    added fallback logo, minor adjustments

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import styles from './LandingPage.module.scss'
 import logoMobile from '../../images/logoMobile.svg'
 import logo from '../../images/logo.svg'
+import logoFallback from '../../images/logo.png'
 import arrow from '../../images/Icon awesome-long-arrow-alt-down.svg'
 
 function LandingPage() {
@@ -18,14 +19,14 @@ function LandingPage() {
                         media="(min-width:481px)"
                         srcSet={logo}
                     />
-                    <img src={logo} alt="poké queen logo" />
+                    <img src={logoFallback} alt="poké queen logo" />
                     {/*//den här ska jag byta ut mot en png-bild för gamla browsers*/}
                 </picture>
                 <h2>Om du vill vara trendig - och äta gott!</h2>
             </header>
             <section className={styles.promotionText}>
                 <h1>Grand Opening 23/12</h1>
-                <button className="primary-btn">boka nu</button>
+                <button className="primary-btn">Boka nu</button>
             </section>
             <div className={styles.arrowContainer}>
                 <img
@@ -37,7 +38,7 @@ function LandingPage() {
             <svg width="0" height="0">
                 <defs>
                     <clipPath id="desktop" clipPathUnits="objectBoundingBox">
-                        <path d="m 0,0 0.00390625,0.9453125 c 0,0 0.17125052,0.0744347 0.47460938,0.0449219 C 0.89124947,0.93766195 1.0078125,0.96875 1.0078125,0.96875 L 1,0 Z" />
+                        <path d="M 0,0 3.972864e-4,0.91065274 C 0.33151178,1.1046347 0.43093849,0.9209222 0.99845907,0.93019465 L 1,0.00116994 Z" />
                     </clipPath>
                     <clipPath
                         id="tablet-or-mobile"
