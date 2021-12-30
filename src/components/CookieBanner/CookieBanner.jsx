bc7db2346b548ddda7cb49bf8928adba3d02ab41
Axel Pahne <random@wwho.com>
2021-12-30 17:50:24 +0100

    cookieBanner

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -1,23 +1,30 @@
 import styles from './CookieBanner.module.scss'
 import React, { useState } from 'react'
 function CookieBanner() {
-
     const [display, setDisplay] = useState(true)
-    
 
-    return (
-        display?
-            <div className={styles.CookieBanner}>
-                <p> Poké Queen använder kakor (cookies) för att webbplatsen ska fungera bra för dig. 
+    return display ? (
+        <div className={styles.CookieBanner}>
+            <p>
+                {' '}
+                Poké Queen använder kakor (cookies) för att webbplatsen ska
+                fungera bra för dig.
                 <span>Läs mer om kakor</span>
-                </p>
+            </p>
 
-                <button className={styles.CookiesBtn} onClick={() => setDisplay(false)}> 
-                    Jag förstår
-                </button>
-            
-            </div> 
-        :""
+            <button
+                className={styles.CookiesBtn}
+                onClick={() =>
+                    setTimeout(() => {
+                        setDisplay(false)
+                    }, 500)
+                }
+            >
+                Jag förstår
+            </button>
+        </div>
+    ) : (
+        ''
     )
 }
 
