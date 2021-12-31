265d23583024adf3e3139a615f1f2d087dfe47ff
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-12-31 11:03:03 +0100

    delete px of ratio in width and height of image in html

--- src/components/MealWrapper/MealWrapper.jsx
+++ src/components/MealWrapper/MealWrapper.jsx
@@ -12,8 +12,8 @@ function MealWrapper() {
                         <img
                             className={styles.MealCardPic}
                             src={foodPhoto}
-                            width="180px"
-                            height="180px"
+                            width="180"
+                            height="180"
                             alt="Ohana Bowl"
                         />
                     </div>
@@ -33,8 +33,8 @@ function MealWrapper() {
                         <img
                             className={styles.MealCardPic}
                             src={foodPhoto}
-                            width="180px"
-                            height="180px"
+                            width="180"
+                            height="180"
                             alt="Opae Bowl"
                         />
                     </div>
@@ -54,8 +54,8 @@ function MealWrapper() {
                         <img
                             className={styles.MealCardPic}
                             src={foodPhoto}
-                            width="180px"
-                            height="180px"
+                            width="180"
+                            height="180"
                             alt="Kamalii Bowl"
                         />
                     </div>
